ef7e3bdc74db647f5d828bbbc5df08d45eebf48e
vincentbernardprogit <vincent.bernard.corp@gmail.com>
2026-08-16 13:22:13 +0700

    Add buckethead mole
    To add buckethead mole as a new mole type.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const groundHoles = document.querySelectorAll('.ground-hole')
-const moles = document.querySelectorAll('.mole')
-const highScore = document.querySelector('.high-score')
-const pop = document.querySelector('#pop')
+const groundHoles = document.querySelectorAll(".ground-hole")
+const moles = document.querySelectorAll(".mole")
+const bucketheadMoles = document.querySelectorAll(".buckethead-mole")
+const highScore = document.querySelector(".high-score")
+const pop = document.querySelector("#pop")
 
-let previousGroundHole, gameOver, score
+let previousGroundHole, gameOver, score, moleHitCount = 0
 
 function start() {
 	gameOver = false
@@ -31,23 +32,49 @@ function setMoleAppearanceDuration(min, max) {
 function summonMole() {
 	const currentGroundHole = selectGroundHole(groundHoles)
 	const currentMoleAppearanceDuration = setMoleAppearanceDuration(1000, 5000)
-	currentGroundHole.classList.add('appear')
+
+	const appearIndex = Math.floor(Math.random() * 2)
+	let appear
+	if(appearIndex == 0) appear = "mole-appear"
+	if(appearIndex == 1) appear = "buckethead-mole-appear"
+	currentGroundHole.classList.add(appear)
 
 	setTimeout(() => {
-		currentGroundHole.classList.remove('appear')
-		if (!gameOver) {
-			summonMole()
-		}
+		currentGroundHole.classList.remove(appear)
+		if (!gameOver) summonMole()
 	}, currentMoleAppearanceDuration)
 }
 
 function hitMole() {
 	score++
 	pop.play()
-	this.parentNode.classList.remove('appear')
+	this.parentNode.classList.remove("mole-appear")
 	highScore.textContent = score
 }
 
-moles.forEach(mole => {
-  	mole.addEventListener('click', hitMole)
-})
+function hitBucketheadMole() {
+	moleHitCount++
+	if(moleHitCount == 1) {
+		pop.play()
+		this.style.backgroundImage = "url(images/buckethead_mole_hit.png)"
+		this.style.backgroundSize = "50%"
+		this.style.backgroundRepeat = "no-repeat"
+	}
+	else if(moleHitCount == 2) {
+		pop.play()
+		this.style.backgroundImage = "url(images/mole.png)"
+		this.style.backgroundSize = "60%"
+		this.style.backgroundRepeat = "no-repeat"
+	}
+	else if(moleHitCount == 3) {
+		score++
+		pop.play()
+		this.parentNode.classList.remove("buckethead-mole-appear")
+		highScore.textContent = score
+		moleHitCount = 0
+	}
+}
+
+moles.forEach(mole => {mole.addEventListener("click", hitMole)})
+
+bucketheadMoles.forEach(bucketheadMole => {bucketheadMole.addEventListener("click", hitBucketheadMole)})
