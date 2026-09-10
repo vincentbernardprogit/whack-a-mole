7d2ea41b46eb121dfc36173224ac7ea36d781d15
vincentbernardprogit <vincent.bernard.corp@gmail.com>
2026-09-11 02:47:48 +0700

    Fix bugs 3 1. Stage screen a. Hitting the ground hole increases the score regardless of the animals coming out from the ground hole (FIXED).

--- script.js
+++ script.js
@@ -65,58 +65,69 @@ function summonMole() {
 
 function hitMole() {
 	if (this.parentNode.className == "ground-hole") score += 0
-	else score++
-	pop.play()
-	this.parentNode.classList.remove("mole-appear")
-	highScore.textContent = score
+	else if (this.parentNode.classList == "ground-hole mole-appear") {
+		score++
+		pop.play()
+		this.parentNode.classList.remove("mole-appear")
+		highScore.textContent = score
+	}
 }
 
 function hitConeheadMole() {
-	coneheadMoleHitCount++
-	if(coneheadMoleHitCount == 1){
-		pop.play()
-		this.style.backgroundImage = "url(images/mole.png)"
-		this.style.backgroundSize = "60%"
-		this.style.backgroundRepeat = "no-repeat"
-	}
-  else if(coneheadMoleHitCount == 2){
-		score++
-		pop.play()
-		this.parentNode.classList.remove("conehead-mole-appear")
-		highScore.textContent = score
-		coneheadMoleHitCount = 0
+	if (this.parentNode.className == "ground-hole") score += 0
+	else if (this.parentNode.classList == "ground-hole conehead-mole-appear") {
+		coneheadMoleHitCount++
+		if(coneheadMoleHitCount == 1){
+			pop.play()
+			this.style.backgroundImage = "url(images/mole.png)"
+			this.style.backgroundSize = "60%"
+			this.style.backgroundRepeat = "no-repeat"
+		}
+		else if(coneheadMoleHitCount == 2){
+			score++
+			pop.play()
+			this.parentNode.classList.remove("conehead-mole-appear")
+			highScore.textContent = score
+			coneheadMoleHitCount = 0
+		}
 	}
 }
   
 function hitBucketheadMole() {
-	bucketheadMoleHitCount++
-	if(bucketheadMoleHitCount == 1) {
-		pop.play()
-		this.style.backgroundImage = "url(images/buckethead_mole_hit.png)"
-		this.style.backgroundSize = "50%"
-		this.style.backgroundRepeat = "no-repeat"
-	}
-	else if(bucketheadMoleHitCount == 2) {
-		pop.play()
-		this.style.backgroundImage = "url(images/mole.png)"
-		this.style.backgroundSize = "60%"
-		this.style.backgroundRepeat = "no-repeat"
-	}
-	else if(bucketheadMoleHitCount == 3) {
-		score++
-		pop.play()
-		this.parentNode.classList.remove("buckethead-mole-appear")
-		highScore.textContent = score
-		bucketheadMoleHitCount = 0
+	if (this.parentNode.className == "ground-hole") score += 0
+	else if (this.parentNode.classList == "ground-hole buckethead-mole-appear") {
+		bucketheadMoleHitCount++
+		if(bucketheadMoleHitCount == 1) {
+			pop.play()
+			this.style.backgroundImage = "url(images/buckethead_mole_hit.png)"
+			this.style.backgroundSize = "50%"
+			this.style.backgroundRepeat = "no-repeat"
+		}
+		else if(bucketheadMoleHitCount == 2) {
+			pop.play()
+			this.style.backgroundImage = "url(images/mole.png)"
+			this.style.backgroundSize = "60%"
+			this.style.backgroundRepeat = "no-repeat"
+		}
+		else if(bucketheadMoleHitCount == 3) {
+			score++
+			pop.play()
+			this.parentNode.classList.remove("buckethead-mole-appear")
+			highScore.textContent = score
+			bucketheadMoleHitCount = 0
+		}
 	}
 }
   
 function hitSnake() {
-	score--
-	pop.play()
-	this.parentNode.classList.remove("snake-appear")
-	if (score <= 0) highScore.textContent = 0
-	else highScore.textContent = score
+	if (this.parentNode.className == "ground-hole") score += 0
+	else if (this.parentNode.classList == "ground-hole snake-appear") {
+		score--
+		pop.play()
+		this.parentNode.classList.remove("snake-appear")
+		if (score <= 0) highScore.textContent = 0
+		else highScore.textContent = score
+	}
 }
 
 moles.forEach(mole => {mole.addEventListener("click", hitMole)})
